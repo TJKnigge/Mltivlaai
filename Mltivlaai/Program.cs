bdfaf0b3da151debe636e698b16440dacddc95b8
TJknigge <tjknigge1960@kpnmail.nl>
2021-09-17 20:03:56 +0200

    Add project files.

---
+++ Mltivlaai/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MedewerkersMultivlaai
+{
+    class Medewerker
+    {
+        static void Main(string[] args)
+        {
+
+            for (int n = 0; n <3; n++)
+
+            { 
+
+
+            Console.WriteLine("Naam medewerker: ");
+            var medewerkernaam = Console.ReadLine();
+
+                // Console.WriteLine("Functie: ");
+                // var Functie = Console.ReadLine();
+
+                // Console.WriteLine("Geboorte datum: ");
+                // var geboortedatum = Console.ReadLine();
+
+                // Console.WriteLine("Leeftijd: ");
+                // var leeftijd = Console.ReadLine();
+
+
+                // y = Console.ReadLine();
+                // var str = $"{leeftijd: yyyy - v}";
+
+            }
+        }
+    }
+}
